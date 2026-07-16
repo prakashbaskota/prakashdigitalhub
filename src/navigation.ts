7eb2cb96e745c172cbd342b23a849ad4208ce6c1
Prakash Baskota <prakashbaskota@gmail.com>
2026-07-16 11:54:05 +0300

    animated cards  and button

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,7 +1,11 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getBlogPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
+    {
+      text: 'Home',
+      href: getPermalink('/'),
+    },
     {
       text: 'Services',
       href: getPermalink('/#services'),
